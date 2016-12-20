23a5ce5440f44f2a1aad291adf2ca1163baa1765
徐大腿 <71958032@qq.com>
2016-12-20 23:17:38 +0800

    Initial commit

---
+++ src/Router.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="v-router">
+      <p>
+        <router-link to="/foo">go to foo</router-link>
+        <router-link to="/bar">go to bar</router-link>
+      </p>
+      <router-view></router-view>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#fff;
+    }
+</style>
+<script>
+  export default {
+  name: 'v-router'
+  }
+</script>
